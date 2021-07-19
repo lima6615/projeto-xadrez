eca13a271fe3a7e34c91e4b2df8df8b6e5d2fde3
lima6615 <leo-queiroz1@outlook.com>
2021-07-19 20:31:12 -0300

    Primeira class Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,17 @@
 package application;
 
-public class Program {
+import java.util.Locale;
+import java.util.Scanner;
 
+import boardgame.Position;
+
+public class Program {
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		Position pos = new Position(3, 5);
+		System.out.println(pos);
+		sc.close();
+	}
 }
